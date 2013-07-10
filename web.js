453037d8fb36988c3cd520120c032df98ce4d8d2
Ishwor Thapa <ishworthapa@gmail.com>
2013-07-10 06:25:10 +0000

    changed web.js to display the content of index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+var infile = "index.html";
+
+var buf = fs.readFileSync(infile);
+var bufStr = buf.toString();
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(bufStr);
 });
 
 var port = process.env.PORT || 5000;
